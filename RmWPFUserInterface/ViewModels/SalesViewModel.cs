52932e21365a9698084b472c512e3a4518744bf6
DarshanSudhakar <$K3Ug1i&>
2020-11-16 12:49:37 +0530

    Modifying the data type of ItemQuantity from string to int

--- RmWPFUserInterface/ViewModels/SalesViewModel.cs
+++ RmWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -34,9 +34,9 @@ namespace RmWPFUserInterface.ViewModels
             }
         }
 
-        private string _itemQuantity;
+        private int _itemQuantity;
 
-        public string ItemQuantity
+        public int ItemQuantity
         {
             get { return _itemQuantity; }
             set
