fb52412b0c7194b6c627c3b79ee3209c78363265
DarshanSudhakar <$K3Ug1i&>
2021-09-19 18:33:42 +0530

    Add Saving the cart to the database
    All the changes are necessary to save the cart

--- RmDataManager.Library/DataAccess/ProductData.cs
+++ RmDataManager.Library/DataAccess/ProductData.cs
@@ -18,5 +18,14 @@ namespace RmDataManager.Library.DataAccess
 
             return output;
         }
+        
+        public ProductModel GetProductById(int productId)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetById", new { Id = productId }, "RmData").FirstOrDefault();
+
+            return output;
+        }
     }
 }
---
+++ RmDataManager.Library/DataAccess/SaleData.cs
@@ -0,0 +1,74 @@
+﻿using RmDataManager.Library.Internal.DataAccess;
+using RmDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RmDataManager.Library.DataAccess
+{
+    public class SaleData
+    {
+        
+        public void SaveSale(SaleModel saleInfo, string cashierId)
+        {
+            //TODO: Make this SOLID/DRY/Better
+
+            //Start filling in the sale detail models we will save to the database
+            List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
+            ProductData productData = new ProductData();
+            var taxRate = ConfigHelper.GetTaxRate();
+
+            foreach (var item in saleInfo.SaleDetails)
+            {
+                var detail = new SaleDetailDBModel
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity
+                };
+
+                //Fill in the available information
+                //Get the information about this product
+                var productInfo = productData.GetProductById(item.ProductId);
+
+                if (productInfo == null)
+                {
+                    throw new Exception($"The product ID of {detail.ProductId} could not be found in the database");
+                }
+
+                detail.PurchasePrice = productInfo.RetailPrice * detail.Quantity;
+                detail.Tax = productInfo.IsTaxable ? (detail.PurchasePrice * (taxRate / 100)) : 0;
+
+                details.Add(detail);
+            }
+
+            //Create the Salemodel
+            SaleDBModel sale = new SaleDBModel
+            {
+                SubTotal = details.Sum(x => x.PurchasePrice),
+                Tax = details.Sum(x => x.Tax),
+                CashierId = cashierId
+            };
+
+            sale.Total = sale.SubTotal + sale.Tax;
+
+            //Save the sale model
+            SqlDataAccess sql = new SqlDataAccess();
+            sql.SaveData<SaleDBModel>("RmData.dbo.spSale_Insert", sale, "RmData");
+
+            //Get the ID from the sale model
+            sale.Id = sql.LoadData<int, dynamic>("dbo.spSale_Lookup", new { CashierId = sale.CashierId, SaleDate = sale.SaleDate }, "RmData").FirstOrDefault();
+
+            //Finish filling in the sale detail models
+            foreach (var item in details)
+            {
+                item.SaleId = sale.Id;
+                
+                //Save the sale detail models
+                sql.SaveData("dbo.spSaleDetails_Insert", item, "RmData");
+            }
+            
+        }
+    }
+}
