80ee1c86d57dc20fe1d49b3eb5266f53fe60f54c
Darshan Sudhakar <darshanraj.shetty@gmail.com>
2021-09-26 21:27:46 +0530

    Adding and removing user roles (#24)
    Created a endpoint API for adding a role to user
    Created a endpoint API for removing a role to user
    
    Co-authored-by: DarshanSudhakar <$K3Ug1i&>

--- RmDataManager/Controllers/UserController.cs
+++ RmDataManager/Controllers/UserController.cs
@@ -55,5 +55,46 @@ namespace RmDataManager.Controllers
                 return output;
             }
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/User/Admin/GetAllRoles")]
+        public Dictionary<string, string> GetAllRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id, x => x.Name);
+
+                return roles;
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/AddRole")]
+        public void AddRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/RemoveRole")]
+        public void RemoveRole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+            }
+        }
     }
 }
