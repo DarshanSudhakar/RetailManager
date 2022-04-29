e38148ae47a2d12e874f25839c326943c2af954f
Darshan Sudhakar <darshanraj.shetty@gmail.com>
2022-04-29 10:21:40 +0530

    Private/darshan/37 planning phase 2 (#31)
    * Removing the RmDataManger which is the old WebAPI
    
    * Removing Swashbuckle.Core nuget package
    Connecting WPF to right web API URL
    Fixing the issue where cart continues to stay where it is even after logout
    
    * Adding roadmap on phase 2
    
    Co-authored-by: DarshanSudhakar <$K3Ug1i&>

--- RmDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace RmDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- RmDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RmDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
