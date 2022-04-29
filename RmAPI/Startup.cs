78041e7657ceb88190115b0a9557d200facf8cf2
DarshanSudhakar <$K3Ug1i&>
2022-04-29 21:08:54 +0530

    Adding JWT secruity key to configuration

--- RmAPI/Startup.cs
+++ RmAPI/Startup.cs
@@ -58,7 +58,7 @@ namespace RmAPI
                     jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
